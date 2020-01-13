978729a30692a50a9eee1a3b615567ae37fdd2d7
Alex Miller <alexmillerprojects@gmail.com>
2020-01-13 15:13:52 -0800

    finished main assignment, working on stretch

--- js/index.js
+++ js/index.js
@@ -40,3 +40,90 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navItems = document.querySelectorAll('nav a');
+navItems.forEach((item) => {
+  item.style.color = '#b0b0b0';
+})
+
+//figure out how to loop this...?
+navItems[0].textContent = siteContent.nav["nav-item-1"];
+navItems[1].textContent = siteContent.nav["nav-item-2"];
+navItems[2].textContent = siteContent.nav["nav-item-3"];
+navItems[3].textContent = siteContent.nav["nav-item-4"];
+navItems[4].textContent = siteContent.nav["nav-item-5"];
+navItems[5].textContent = siteContent.nav["nav-item-6"];
+
+
+//Call to Action Banner Section
+let bannerImg = document.querySelector('#cta-img')
+bannerImg.setAttribute('src', siteContent['cta']['img-src'])
+
+let ctaText = document.querySelector('h1')
+ctaText.textContent = siteContent['cta']['h1']
+ctaText.style.fontFamily = 'Bangers, cursive'
+ctaText.style.letterSpacing = '1px'
+ctaText.style.marginBottom = '15px'
+
+let ctaButton = document.querySelector('.cta-text :last-child')
+ctaButton.textContent = siteContent['cta']['button']
+
+
+//Top Content
+const topContent = document.querySelector('.top-content')
+topContent.style.display = 'flex';
+
+let featuresTitle = document.querySelector('.top-content .text-content h4')
+featuresTitle.textContent = siteContent['main-content']['features-h4']
+
+let featuresText = document.querySelector('.top-content .text-content p')
+featuresText.textContent = siteContent['main-content']['features-content']
+
+let aboutTitle = document.querySelector('.top-content .text-content ~ .text-content h4')
+aboutTitle.textContent = siteContent['main-content']['about-h4']
+
+let aboutText = document.querySelector('.top-content .text-content ~ .text-content p')
+aboutText.textContent = siteContent['main-content']['about-content']
+
+//Divider Image
+let dividerImg = document.querySelector('#middle-img')
+dividerImg.setAttribute('src', siteContent['main-content']['middle-img-src'])
+
+//Bottom Content
+const bottomContent = document.querySelector('.bottom-content')
+bottomContent.style.display = 'flex';
+
+let servicesTitle = document.querySelector('.bottom-content .text-content h4')
+servicesTitle.textContent = siteContent['main-content']['services-h4']
+
+let servicesText = document.querySelector('.bottom-content .text-content p')
+servicesText.textContent = siteContent['main-content']['services-content']
+
+let productTitle = document.querySelector('.bottom-content .text-content ~ .text-content h4')
+productTitle.textContent = siteContent['main-content']['about-h4']
+
+let productText = document.querySelector('.bottom-content .text-content ~ .text-content p')
+productText.textContent = siteContent['main-content']['about-content']
+
+let visionTitle = document.querySelector('.bottom-content .text-content ~ :last-child h4')
+visionTitle.textContent = siteContent['main-content']['about-h4']
+
+let visionText = document.querySelector('.bottom-content :last-child p')
+visionText.textContent = siteContent['main-content']['about-content']
+
+//Contact Section
+let contactTitle = document.querySelector('.contact h4')
+contactTitle.textContent = siteContent['contact']['contact-h4']
+
+let cAddress = document.querySelector('.contact p')
+cAddress.textContent = siteContent['contact']['address']
+
+let cPhone = document.querySelector('.contact p ~ p')
+cPhone.textContent = siteContent['contact']['phone']
+
+let cEmail = document.querySelector('.contact :last-child')
+cEmail.textContent = siteContent['contact']['email']
+
+//Footer
+let copyright = document.querySelector('footer p')
+copyright.textContent = siteContent['footer']['copyright']
