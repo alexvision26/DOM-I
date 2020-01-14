303f590e80b566489f4ab989feec94c8954a0195
Alex Miller <alexmillerprojects@gmail.com>
2020-01-13 16:38:01 -0800

    finished project and one stretch

--- js/index.js
+++ js/index.js
@@ -43,16 +43,31 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 let navItems = document.querySelectorAll('nav a');
 navItems.forEach((item) => {
-  item.style.color = '#b0b0b0';
+  item.style.color = 'green';
 })
 
-//figure out how to loop this...?
 navItems[0].textContent = siteContent.nav["nav-item-1"];
 navItems[1].textContent = siteContent.nav["nav-item-2"];
 navItems[2].textContent = siteContent.nav["nav-item-3"];
 navItems[3].textContent = siteContent.nav["nav-item-4"];
 navItems[4].textContent = siteContent.nav["nav-item-5"];
 navItems[5].textContent = siteContent.nav["nav-item-6"];
+
+const teamBtn = document.createElement('a')
+teamBtn.textContent = 'Team'
+teamBtn.style.color = 'green'
+teamBtn.style.cursor = 'pointer'
+teamBtn.classList.add('a')
+
+const blog = document.createElement('a')
+blog.textContent = 'Blog'
+blog.style.color = 'green'
+blog.style.cursor = 'pointer'
+blog.classList.add('a')
+
+const newMenu = document.querySelector('nav')
+newMenu.appendChild(teamBtn)
+newMenu.prepend(blog)
 
 
 //Call to Action Banner Section
@@ -64,9 +79,46 @@ ctaText.textContent = siteContent['cta']['h1']
 ctaText.style.fontFamily = 'Bangers, cursive'
 ctaText.style.letterSpacing = '1px'
 ctaText.style.marginBottom = '15px'
+ctaText.style.wordSpacing = '880px'
+
+let ctaContainer = document.querySelector('.cta-text')
+ctaContainer.style.margin = '0px'
+ctaContainer.style.paddingLeft = '100px'
 
 let ctaButton = document.querySelector('.cta-text :last-child')
 ctaButton.textContent = siteContent['cta']['button']
+
+
+//Stretch?
+let newCont = document.querySelector('.container')
+newCont.style.width = "100%"
+
+let maxMainWidth = document.querySelector('.main-content')
+maxMainWidth.style.width = '100%'
+maxMainWidth.style.border = '0'
+
+maxMainWidth.style.backgroundColor = '#E3E3E6'
+
+let topContWidth = document.querySelector('.top-content')
+topContWidth.style.width = '64%'
+topContWidth.style.margin = '0 auto'
+
+let botContWidth = document.querySelector('.bottom-content')
+botContWidth.style.width = '64%'
+botContWidth.style.margin = '0 auto'
+
+let maxHeadWidth = document.querySelector('header')
+maxHeadWidth.style.justifyContent = 'center'
+maxHeadWidth.style.backgroundColor = '#E3E3E6'
+maxHeadWidth.style.paddingBottom = '3%'
+
+let maxCTA = document.querySelector('.cta')
+maxCTA.style.width = '66%'
+maxCTA.style.margin = '52px auto'
+
+let maxContact = document.querySelector('.contact')
+maxContact.style.width = '64%'
+maxContact.style.margin = '3% auto'
 
 
 //Top Content
@@ -127,3 +179,4 @@ cEmail.textContent = siteContent['contact']['email']
 //Footer
 let copyright = document.querySelector('footer p')
 copyright.textContent = siteContent['footer']['copyright']
+copyright.style.margin = '0'
